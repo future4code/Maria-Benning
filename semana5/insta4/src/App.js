2955b5d340433a98c52fe4ca9177757614a7b8ad
mariabenning <mariabenning15@gmail.com>
2021-07-14 16:34:54 -0300

    semana5-aula19

---
+++ semana5/insta4/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+import Post2 from './components/Post/Post2';
+import Post3 from './components/Post/Post3';
+
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+        <Post2
+          nomeUsuario={'Maria'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+         <Post3
+          nomeUsuario={'Mariana'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+      </MainContainer>
+    );
+  }
+}
+
+
+
+
+export default App;
