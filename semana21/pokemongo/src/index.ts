08b4f6cac354188c4c98094820c93cb02c8d0038
mariabenning <mariabenning15@gmail.com>
2021-11-01 17:33:04 -0300

    ajustando servidor

--- semana21/pokemongo/src/index.ts
+++ semana21/pokemongo/src/index.ts
@@ -1,6 +1,9 @@
 import express, {Express} from 'express'
 import cors from 'cors'
 import { AddressInfo } from "net";
+import knex from "knex";
+
+import dotenv from "dotenv";
 
 
 
@@ -9,7 +12,31 @@ const app: Express = express();
 app.use(express.json());
 app.use(cors());
 
+dotenv.config();
 
+
+
+export const connection = knex({
+	client: "mysql",
+	connection: {
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  }
+});
+
+
+app.get('/ping', () => {
+
+try {
+   console.log('pong')
+}
+catch (error) {
+   console.log(error)
+}
+})
 
 
 
@@ -24,7 +51,7 @@ app.use(cors());
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
        const address = server.address() as AddressInfo;
-       console.log(`Server is running in http://localhost: ${address.port}`);
+       console.log(` O Servidor está em  http://localhost: ${address.port}`);
     } else {
        console.error(`Failure upon starting server.`);
     }
